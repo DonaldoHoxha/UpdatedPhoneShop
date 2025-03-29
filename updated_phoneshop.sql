d1a69ebca4f3734bf37c29fb2db04127d6f86ba5
DonaldoHoxha <149370592+DonaldoHoxha@users.noreply.github.com>
2025-03-29 16:13:26 +0100

    Update updated_phoneshop.sql

--- updated_phoneshop.sql
+++ updated_phoneshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2025 at 01:55 PM
+-- Generation Time: Mar 29, 2025 at 04:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,7 +41,7 @@ CREATE TABLE `administrator_user` (
 
 CREATE TABLE `cart` (
   `user_id` int(11) NOT NULL,
-  `warehouse_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
   `quantity` int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -49,15 +49,42 @@ CREATE TABLE `cart` (
 -- Dumping data for table `cart`
 --
 
-INSERT INTO `cart` (`user_id`, `warehouse_id`, `quantity`) VALUES
-(4, 1, 2),
-(4, 3, 1),
+INSERT INTO `cart` (`user_id`, `product_id`, `quantity`) VALUES
+(4, 1, 4),
+(4, 2, 3),
+(4, 3, 3),
+(4, 5, 1),
 (5, 2, 1),
 (5, 3, 2),
 (5, 4, 1),
 (6, 1, 4),
 (6, 2, 2),
 (6, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `brand` varchar(100) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `brand`, `price`, `quantity`) VALUES
+(1, 'iPhone 15 Pro', 'Apple', 1199, 10),
+(2, 'Samsung Galaxy A24', 'Samsung', 500, 15),
+(3, 'xiaomi 1', 'xiaomi', 200, 500),
+(4, 'huawei', 'huawei', 40, 2),
+(5, 'iphone 12 pro ', 'apple', 600, 30);
 
 -- --------------------------------------------------------
 
@@ -85,31 +112,6 @@ INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
 (7, 'don3', '$2y$10$juoNX/rAkXXX8TgoOUO8TOc9RY38yEPnkDiWehlyikxGFmaKDL4Wu', 'don3@gmail.com'),
 (8, 'qiu1', '$2y$10$RPUKv1QFcrorE1S92bKJyOXcWVUlF/ORq/kKpShiXHII43kb84flO', 'qiu1@gmail.com');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `warehouse`
---
-
-CREATE TABLE `warehouse` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `brand` varchar(100) NOT NULL,
-  `price` float NOT NULL,
-  `quantity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `warehouse`
---
-
-INSERT INTO `warehouse` (`id`, `name`, `brand`, `price`, `quantity`) VALUES
-(1, 'iPhone 15 Pro', 'Apple', 1199, 10),
-(2, 'Samsung Galaxy A24', 'Samsung', 500, 15),
-(3, 'xiaomi 1', 'xiaomi', 200, 500),
-(4, 'huawei', 'huawei', 40, 2),
-(5, 'iphone 12 pro ', 'apple', 600, 30);
-
 --
 -- Indexes for dumped tables
 --
@@ -124,8 +126,14 @@ ALTER TABLE `administrator_user`
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
-  ADD PRIMARY KEY (`user_id`,`warehouse_id`),
-  ADD KEY `warehouse_id` (`warehouse_id`);
+  ADD PRIMARY KEY (`user_id`,`product_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -134,12 +142,6 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `password` (`password`);
-
---
--- Indexes for table `warehouse`
---
-ALTER TABLE `warehouse`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -152,16 +154,16 @@ ALTER TABLE `administrator_user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `warehouse`
---
-ALTER TABLE `warehouse`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -172,7 +174,7 @@ ALTER TABLE `warehouse`
 --
 ALTER TABLE `cart`
   ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
