c3739cb1c5b331dd88bb9a6898395faa875e81e9
DonaldoHoxha <149370592+DonaldoHoxha@users.noreply.github.com>
2025-04-15 10:49:59 +0200

    Update orders.php

--- qiu_phoneshop/main_page/orders.php
+++ qiu_phoneshop/main_page/orders.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profilo</title>
+    <title>Orders</title>
     <style>
         table,
         th,
