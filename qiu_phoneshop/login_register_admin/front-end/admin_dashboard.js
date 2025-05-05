e2b73cfa37b81c4f3b7c9d9c95a1615639e3dc87
Q1uM <qiu2797223631@gmail.com>
2025-05-05 18:58:26 +0200

    click effect for admin_dashboard sidebar

--- qiu_phoneshop/login_register_admin/front-end/admin_dashboard.js
+++ qiu_phoneshop/login_register_admin/front-end/admin_dashboard.js
@@ -1,19 +1,23 @@
 const sidebarSection = document.querySelectorAll(".sidebar a");
 const dashboardSection = document.getElementById("dashboard");
 
-sidebarSection.addEventListener("click", function () {
-    sidebarSection.forEach((section) => {
-        section.classList.remove("active");
+sidebarSection.forEach((link) => {
+    link.addEventListener("click", function () {
+        sidebarSection.forEach((link) => {
+            link.classList.remove("active");
+        });
+        this.classList.add("active");
+
     });
-    this.classList.add("active");
 
     switch (this.id) {
         case "users":
             showUsers();
             break;
     }
-
 });
+
+
 
 function showUsers(){
 
